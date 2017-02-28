58c7bab247ac2d989a3cbb7189cf04f92cc41cd9
רועי צחור <roy.zahor@gmail.com>
2017-02-28 11:24:20 +0200

    basic frontend working

--- public/javascripts/calc.js
+++ public/javascripts/calc.js
@@ -1,9 +1,13 @@
+var calculatorScreen = $('#calculatorScreen');
+var input = $('#input');
+
+var currentExpression = '';
+var viewOnScreen = '';
+
 $(function () {
     "use strict";
 
     // for better performance - to avoid searching in DOM
-    var content = $('#content');
-    var input = $('#input');
     var status = $('#status');
 
     // my color assigned by the server
@@ -16,7 +20,7 @@ $(function () {
 
     // if browser doesn't support WebSocket, just show some notification and exit
     if (!window.WebSocket) {
-        content.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
+        calculatorScreen.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
         + 'support WebSockets.'} ));
         input.hide();
         $('span').hide();
@@ -34,7 +38,7 @@ $(function () {
 
     connection.onerror = function (error) {
         // just in there were some problems with conenction...
-        content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
+        calculatorScreen.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
         + 'connection or the server is down.' } ));
     };
 
@@ -60,13 +64,13 @@ $(function () {
         } else if (json.type === 'history') { // entire message history
             // insert every single message to the chat window
             for (var i=0; i < json.data.length; i++) {
-                addMessage(json.data[i].author, json.data[i].text,
-                    json.data[i].color, new Date(json.data[i].time));
+                //addMessage(json.data[i].author, json.data[i].text,
+                //    json.data[i].color, new Date(json.data[i].time));
             }
         } else if (json.type === 'message') { // it's a single message
             input.removeAttr('disabled'); // let the user write another message
-            addMessage(json.data.author, json.data.text,
-                json.data.color, new Date(json.data.time));
+            //addMessage(json.data.author, json.data.text,
+            //    json.data.color, new Date(json.data.time));
         } else {
             console.log('Hmm..., I\'ve never seen JSON like this: ', json);
         }
@@ -111,10 +115,33 @@ $(function () {
     /**
      * Add message to the chat window
      */
-    function addMessage(author, message, color, dt) {
-        content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
-            + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
-            + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
-            + ': ' + message + '</p>');
-    }
+
 });
+
+function updateCalc( input_str, color, dt) {
+    console.log(input_str);
+    if(input_str === "="){ // evaluate the expression
+        // present the result
+        currentExpression = eval(currentExpression);
+        viewOnScreen = currentExpression;
+    }
+    else if(input_str === "+"){ // present only the plus sign
+        // add plus sign to the expression
+        viewOnScreen = input_str;
+        currentExpression += input_str;
+    }
+    else{ // --> number was pressed
+        // if there was a plus sign presented, present the number pressed
+        //
+        if (viewOnScreen==="+"){
+            viewOnScreen = input_str;
+            currentExpression += input_str;
+        }
+        else{
+            viewOnScreen +=input_str;
+            currentExpression +=input_str;
+        }
+    }
+    calculatorScreen.html($('<p>', { text: viewOnScreen} ));
+
+}
